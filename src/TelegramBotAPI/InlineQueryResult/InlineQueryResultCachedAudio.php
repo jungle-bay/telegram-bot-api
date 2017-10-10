92dc6fede45886d3b646f99942e30e5872e34580
Roma Baranenko <sommelier.jungle@gmail.com>
2017-10-10 16:09:31 +0300

    fix bug, delete method

--- src/TelegramBotAPI/InlineQueryResult/InlineQueryResultCachedAudio.php
+++ src/TelegramBotAPI/InlineQueryResult/InlineQueryResultCachedAudio.php
@@ -155,7 +155,7 @@ class InlineQueryResultCachedAudio extends InlineQueryResult {
         }
 
         if (isset($this->replyMarkup)) {
-            $data['reply_markup'] = $this->getReplyMarkup()->arraySerialize();
+            $data['reply_markup'] = $this->getReplyMarkup();
         }
 
         if (isset($this->inputMessageContent)) {
