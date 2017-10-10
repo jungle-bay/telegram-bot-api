4416a7040a4e1a326ddcf502e5f38729ed946b72
Roma Baranenko <sommelier.jungle@gmail.com>
2017-10-10 16:13:36 +0300

    delete parallel

--- src/TelegramBotAPI/InlineQueryResult/InlineQueryResultAudio.php
+++ src/TelegramBotAPI/InlineQueryResult/InlineQueryResultAudio.php
@@ -225,7 +225,7 @@ class InlineQueryResultAudio extends InlineQueryResult {
         }
 
         if (isset($this->replyMarkup)) {
-            $data['reply_markup'] = $this->getReplyMarkup()->arraySerialize();
+            $data['reply_markup'] = $this->getReplyMarkup();
         }
 
         if (isset($this->inputMessageContent)) {
