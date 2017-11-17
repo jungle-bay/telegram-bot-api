1daf16fd81bf83b7347f9152f63c28088c81d2db
Roma Baranenko <sommelier.jungle@gmail.com>
2017-11-17 21:11:03 +0200

    add obj update to exception

--- src/TelegramBotAPI/Exception/TelegramBotAPIException.php
+++ src/TelegramBotAPI/Exception/TelegramBotAPIException.php
@@ -4,12 +4,33 @@ namespace TelegramBotAPI\Exception;
 
 
 use Exception;
+use TelegramBotAPI\Types\Update;
 
 /**
  * @package TelegramBotAPI\Exception
  * @author Roma Baranenko <jungle.romabb8@gmail.com>
  */
 class TelegramBotAPIException extends Exception {
+
+    /**
+     * @var Update $update
+     */
+    private $update;
+
+
+    /**
+     * @return Update
+     */
+    public function getUpdate() {
+        return $this->update;
+    }
+
+    /**
+     * @param Update $update
+     */
+    public function setUpdate($update) {
+        $this->update = $update;
+    }
 
     /**
      * @param string $message
